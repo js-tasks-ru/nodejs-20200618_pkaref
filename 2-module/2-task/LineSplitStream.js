d116268aac343fc3b7f4756ec3f4b61dd4baff5c
pavelkarev <pavel.karev@pegast.ru>
2020-07-16 02:42:27 +0300

    Add LineSplitStream

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -2,14 +2,42 @@ const stream = require('stream');
 const os = require('os');
 
 class LineSplitStream extends stream.Transform {
+  #line = '';
+
   constructor(options) {
     super(options);
   }
 
   _transform(chunk, encoding, callback) {
+    let string = chunk.toString();
+    let lines;
+
+    if (string.endsWith(os.EOL)) {
+      lines = string.split(os.EOL);
+      lines[0] = this.#line + lines[0];
+      this.#line = '';
+    } else if (string.includes(os.EOL)) {
+      lines = string.split(os.EOL);
+      lines[0] = this.#line + lines[0];
+      this.#line = lines.pop();
+    } else {
+      this.#line += string;
+    }
+
+    if (lines) {
+      lines.forEach((line) => {
+        this.push(line);
+      });
+    }
+
+    callback();
   }
 
   _flush(callback) {
+    if (this.#line) {
+      this.push(this.#line);
+    }
+    callback();
   }
 }
 
